e21854c57178984c3b1a45988ad3ee40497bb2de
Chidinma Umenwofor-Nweze <57571222+ChidiNweze@users.noreply.github.com>
2020-04-01 01:03:55 -0400

    Added assets and files.

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Chidinma Umenwofor-Nweze on 2020-03-31.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //enail address of sender
+    let body: String
+}
